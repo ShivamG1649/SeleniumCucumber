2f62d72f447b7c15b1fc2620bafefd3d978255dd
shivam <gulati.shivam2008@gmail.com>
2024-12-12 22:08:29 +0530

    commit changes1

---
+++ src/test/java/TestDemo.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestDemo {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("Https://google.com");
+        driver.findElement(By.xpath("//textarea[@id='APjFqb']")).sendKeys("sfdsd");
+
+    }
+}
