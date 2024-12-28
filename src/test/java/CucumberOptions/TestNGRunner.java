114ca8078633a8e862c002e82be894939b031ba8
rsita162010 <rsita16.2010@gmail.com>
2024-12-28 17:40:27 -0600

    Login page and Reset password test cases added

--- src/test/java/CucumberOptions/TestNGRunner.java
+++ src/test/java/CucumberOptions/TestNGRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/java/features",
-glue ="stepDefinitions",monochrome = true )
+glue ="stepDefinitions",tags = "@OrangeHRM7", monochrome = true )
 
 public class TestNGRunner extends AbstractTestNGCucumberTests {
 
--- src/test/java/stepDefinitions/LoginPageStepDefinition.java
+++ src/test/java/stepDefinitions/LoginPageStepDefinition.java
@@ -1,4 +1,5 @@
 package stepDefinitions;
+
 import Utils.TestContext;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -8,6 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,7 +19,7 @@ import java.util.List;
 
 public class LoginPageStepDefinition {
 
-   WebDriver driver =TestContext.driver;
+    WebDriver driver = TestContext.driver;
 
 //    public LoginPageStepDefinition(TestContext testContext)
 //    {
@@ -31,10 +33,15 @@ public class LoginPageStepDefinition {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-        String expectedTitle="OrangeHRM";
-        String actualTitle=driver.getTitle();
+
+    }
+
+    @Then("Verify title of the page")
+    public void verify_title_of_the_page() {
+        String expectedTitle = "OrangeHRM";
+        String actualTitle = driver.getTitle();
         System.out.println(actualTitle);
-        Assert.assertEquals(actualTitle,expectedTitle);
+        Assert.assertEquals(actualTitle, expectedTitle);
     }
 
 
@@ -52,6 +59,11 @@ public class LoginPageStepDefinition {
 //        driver.findElement(By.className(""));
 //        driver.findElement(By.linkText("http://www.orangehrm.com"));
 
+    }
+
+    @When("user enter invalid password")
+    public void user_enter_invalid_password() {
+        driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("admin1234");
     }
 
     @When("user click on login button")
@@ -78,8 +90,42 @@ public class LoginPageStepDefinition {
 
     @Then("verify the error message")
     public void verify_the_error_message() {
-        boolean status = driver.findElement(By.xpath("//p[text()='Invalid credentials']")).isDisplayed();
-        Assert.assertTrue(status);
+        boolean OraneHRMLogo = driver.findElement(By.xpath("//img[@alt='client brand banner']")).isDisplayed();
+        Assert.assertTrue(OraneHRMLogo);
+    }
+
+    @When("User clicked Forgot Your Password link")
+    public void user_clicked_forgot_your_password_link() {
+        driver.findElement(By.xpath("//p[text()='Forgot your password? ']")).click();
+    }
+
+    @When("Verify user navigated to Reset Password page")
+    public void verify_user_navigated_to_reset_password_page() {
+        String expectedTitle = "Reset Password";
+        String actualTitle = driver.findElement(By.xpath("//h6[text()='Reset Password']")).getText();
+        Assert.assertEquals(actualTitle, expectedTitle, "title is not matched");
+        System.out.println("User successfully navigated to Reset Password page");
+    }
+
+    @When("User enter {string} and click on Reset password")
+    public void user_enter_and_click_on_reset_password(String string) {
+        driver.findElement(By.name("username")).sendKeys(string);
+        driver.findElement(By.xpath("//button[text()=' Reset Password ']")).click();
+    }
+
+    @When("Verify User navigated to Reset password Successful page")
+    public void verify_user_navigated_to_reset_password_successful_page() {
+        String expectedTitle = "Reset Password link sent successfully";
+        String actualTitle = driver.findElement(By.xpath("//h6[text()='Reset Password link sent successfully']")).getText();
+        Assert.assertEquals(actualTitle, expectedTitle);
+        System.out.println("User successfully navigated to Reset Password link sent successfully page");
+    }
+
+    @When("^user enter \"(.*?)\" and \"(.*?)\"$")
+    public void user_enter(String username ,String password) throws InterruptedException {
+        driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys(username);
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys(password);
     }
 
 
